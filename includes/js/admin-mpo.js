98c8e13512800ee2e3766792486ea35e5b093341
Nelson Alonso <nalonsopress@gmail.com>
2019-06-03 15:12:20 -0400

    Fixing some distortion at certain widths on the dashboard

--- includes/js/admin-mpo.js
+++ includes/js/admin-mpo.js
@@ -5,7 +5,7 @@ jQuery(function($){
       'action' : 'hc_styles_fetch_society',
       },
     success:function(data) {
-	   $("label[for='blog-public']").html('Public and allow search engines to index this site. <br>Note: it is up to search engines to honor your request. The site will appear in public listings around Humanities Commons.');
+	   $("label[for='blog-public']").html('Public and allow search engines to index this site. <br>Note: it is up to search engines to honor your request. <br>The site will appear in public listings around Humanities Commons.');
 	   $("label[for='blog-norobots']").html('Public but discourage search engines from index this site. <br>Note: this option does not block access to your site — it is up to search engines to honor your request.<br> The site will appear in public listings around Humanities Commons.');
 	   $(".option-site-visibility p.description").remove();
 	   $("label[for='blog-norobots']").after("<br />");
