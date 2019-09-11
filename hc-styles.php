bb132d3a76dde6536fe8ec5f13362d98a774770b
Joseff Betancourt <jbetancourt@mla.org>
2019-09-11 10:25:30 -0400

    Fixed email to e-mail.

--- hc-styles.php
+++ hc-styles.php
@@ -28,7 +28,9 @@ if('dispatch' === $theme_name ) {
 }
 
 
-//add_filter( 'comment_form_defaults', function ( $args ) {
-//	$args['fields']['comment-form-notes'] .= 'Your e-mail address will not be published.';
-//	return $args;
-//} );
+add_filter( 'comment_form_defaults', function ( $args ) {
+	// i.e. different themes may have different form structures.
+	// 15 zine uses comment-form which is not using hte hook system so not affected.
+	$args['comment_notes_before'] = "<p class=\"comment-notes\"><span id=\"email-notes\">Your e-mail address will not be published.</span> Required fields are marked <span class=\"required\">*</span>.</p>";
+	return $args;
+} );
